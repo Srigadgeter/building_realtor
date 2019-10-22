33fccb0d7aa1a7d2040f1ce9fe6d49471701ff7a
Srigadgeter <sridhar.t@ezedox.com>
2019-10-22 20:03:47 +0530

    postgres db configured

--- br/settings.py
+++ br/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'br.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'realtordb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres'
     }
 }
 
