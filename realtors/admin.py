92e670bb1985af6701b9cde1adf6378f88aa3057
Srigadgeter <sridhar.t@ezedox.com>
2019-10-23 01:15:17 +0530

    admin page customized

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Realtor
 
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_per_page = 2
 
-admin.site.register(Realtor)
+
+admin.site.register(Realtor, RealtorAdmin)
