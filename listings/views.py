a7fc773d0225387cafac051d2381f7492b67ac73
Srigadgeter <sridhar.t@ezedox.com>
2019-11-15 19:11:29 +0530

    context in the listing details page all changed to dynamic context

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Listing
@@ -14,11 +14,19 @@ def index(request):
     context = {
         'listings': paged_listings
     }
+
     return render(request, 'listings/listings.html', context)
 
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+
+    context = {
+        'listing': listing,
+        'myrange': range(1, 6)
+    }
+
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
