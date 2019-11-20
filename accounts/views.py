2c3e25d89783992653865a9acc65b471e5e124c5
Srigadgeter <sridhar.t@ezedox.com>
2019-11-20 17:18:18 +0530

    login logic added and redirecting to dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 
 
 def register(request):
@@ -50,7 +50,22 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        return redirect('index')
+        data = request.POST
+        username = data['username']
+        password = data['password']
+
+        user = auth.authenticate(
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in successfully')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
